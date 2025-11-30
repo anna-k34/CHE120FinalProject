d36bb5e7d27126fcf85cf58b193c780f412ee99a
Anna Kelley <anna.kelleyy@outlook.com>
2025-11-29 19:07:05 -0500

    anna commit-levels finished

--- CHE120FinalProjectModifiedCode.py
+++ CHE120FinalProjectModifiedCode.py
@@ -35,18 +35,18 @@ scoreX = 5
 scoreY = 5
 font = pygame.font.Font('freesansbold.ttf', 20)
 level=1
-levelScores=[50,100,400]
+levelScores=[10,25,40]
 # Game Over
 game_over_font = pygame.font.Font('freesansbold.ttf', 64)
 def levelComplete():
-    text=font.render("LEVEL " + str(level) + " Complete!", True, (255,255,255))
-    screen.blit(text,(200,260))
+    text=game_over_font.render("Level " + str(level) + " Complete!", True, (255,255,255))
+    screen.blit(text,(150,260))
     if level==1:
         textMessage=font.render("Watch out, the aliens are getting faster", True,(255,255,255) )
-        screen.blit(textMessage,(180,300))
+        screen.blit(textMessage,(200,400))
     elif level==2:
         textMessage=font.render("In level 3, don't hit the bombs!", True,(255,255,255) )
-        screen.blit(textMessage,(180,300))
+        screen.blit(textMessage,(200,400))
         
     pygame.display.update()
     
@@ -60,7 +60,7 @@ def game_over():
     game_over_text = game_over_font.render("GAME OVER",
                                            True, (255,255,255))
     screen.blit(game_over_text, (190, 250))
-
+    pygame.time.delay(1000)
 
 
 playerImageUnscaled = pygame.image.load('spaceship.png')
@@ -68,7 +68,10 @@ playerImage=pygame.transform.scale(playerImageUnscaled,(100,100))
 player_X = 370
 player_Y = 490
 player_Xchange = 0
-
+explosion_X = 0
+explosion_Y = 0
+explosion_radius = 0
+explosion_active = False
 invaderImage = []
 invader_X = []
 invader_Y = []
@@ -86,23 +89,23 @@ def bombsCreate(no_of_bombs):
     for num in range(no_of_bombs):
         bombs.append(bombImage)
         bomb_X.append(random.randint(60,737))
-        bomb_Y.append(random.randint(0, 0))
-        bomb_Ychange.append(0.6)
-    
+        bomb_Y.append(random.randint(50, 300))       
+        bomb_Ychange.append(0)                        
+        bomb_Xchange.append(random.uniform(0.3, 1)) 
 for num in range(no_of_invaders):
     alienImg=pygame.image.load('alien.png')
     alienImg = pygame.transform.scale(alienImg, (40, 40))   
     invaderImage.append(alienImg)
     invader_X.append(random.randint(64, 737))
-    invader_Y.append(random.randint(30, 180))
+    invader_Y.append(random.randint(20, 160))
     invader_Xchange.append(0.6)
     invader_Ychange.append(50)
 
 
 bulletImage = pygame.image.load('bullet.png')
 bulletImage=pygame.transform.scale(bulletImage,(20,30))
-bullet_X = 0
-bullet_Y = 500
+bullet_X = 400
+bullet_Y = 490
 bullet_Xchange = 0
 bullet_Ychange = 3
 bullet_state = "rest"
@@ -134,11 +137,10 @@ def isCollision(x1, x2, y1, y2):
     '''AK the distance between the two is measured by squaring the 
     distance between each axis variable and then taking the square root of these 
     added together'''
-    
     distance = math.sqrt((math.pow(x1 - x2,2)) +
                          (math.pow(y1 - y2,2)))
     #AkIf the distance between the two is less or equal to 50, a colllision is detected
-    if distance <= 50:
+    if distance <= 30:
         return True
     else:
         return False
@@ -178,19 +180,20 @@ while running:
             if event.key == pygame.K_SPACE:
               
                 if bullet_state == "rest":
-                    bullet_X = player_X
+                    bullet_X = player_X + 15
+                    bullet_Y=player_Y
                     bullet(bullet_X, bullet_Y)
         if event.type == pygame.KEYUP:
             player_Xchange = 0
 
-    if score_val>=levelScores[level-1]:
+    if level <= len(levelScores) and score_val>=levelScores[level-1]:
         levelComplete()
         level+=1
         for i in range(no_of_invaders):
             invader_Xchange[i]+=0.3
             invader_Ychange[i]+=5
         if level==3:
-            bombsCreate(5s)
+            bombsCreate(5)
                                                     
     player_X += player_Xchange
     for i in range(no_of_invaders):
@@ -231,12 +234,36 @@ while running:
         invader(invader_X[i], invader_Y[i], i)
 
     
+    if level>=2:
+        for i in range(len(bombs)):
+            bomb_Y[i]+=bomb_Ychange[i]
+            bomb_X[i]+=bomb_Xchange[i]
+            drawBomb(bomb_X[i], bomb_Y[i])
+            if bomb_X[i]<=16 or bomb_X[i]>=750:
+                bomb_Xchange[i]*=-1
+            drawBomb(bomb_X[i],bomb_Y[i])
+            bombCollision=isCollision(bullet_X,bomb_X[i], bullet_Y, bomb_Y[i])
+            gameOverExplosion = False
+            if bombCollision and not gameOverExplosion:
+                explosion_X=bomb_X[i]
+                explosion_Y=bomb_Y[i]
+                explosion_radius=5 
+                explosion_active=True
+            if explosion_active:
+                pygame.draw.circle(screen,(255,0,0), (int(explosion_X), int (explosion_Y)), explosion_radius)
+                explosion_radius+=1
+                if explosion_radius>=1000:
+                    textMessage=game_over_font.render("You Hit the Bomb! Game over", True,(255,255,255) )
+                    screen.blit(textMessage,(100,300))
+                    pygame.display.update()
+                    pygame.time.delay(2000)
+                    game_over()
+                    running=False
 
     if player_X <= 16:
         player_X = 16;
     elif player_X >= 750:
         player_X = 750
-
     
     player(player_X, player_Y)
     show_score(scoreX, scoreY)
